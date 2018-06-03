37fb8be5dc7781ca86181c9ab7486ba852e6eb6f
xingbang <suoxb@foxmail.com>
2018-06-03 21:09:31 +0800

    2018/6/3

--- src/views/photo/Photo.vue
+++ src/views/photo/Photo.vue
@@ -52,8 +52,85 @@
         </div>
       </el-col>
     </el-row>
+
+    <el-dialog :title="dialogStatus" :visible.sync="dialogFormVisible">
+      <el-form ref="dataForm" label-position="right" label-width="90px" :model="temp" >
+        <el-form-item label="标题:" prop="pic_name">
+          <el-input name="pic_name" type="text" placeholder="标题" v-model="temp.pic_name"></el-input>
+        </el-form-item>
+        <el-form-item label="简介:" prop="pic_con">
+          <el-input name="pic_con" type="text" placeholder="简介" v-model="temp.pic_con"></el-input>
+        </el-form-item>
+        <el-form-item label="图片上传:" prop="pic_url">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="点赞数目:" prop="pic_fav">
+          <el-input name="pic_fav" type="text" placeholder="点赞数目" v-model="temp.pic_fav"></el-input>
+        </el-form-item>
+        <el-form-item label="查看数目:" prop="pic_view">
+          <el-input name="pic_view" type="text" placeholder="查看数目" v-model="temp.pic_view"></el-input>
+        </el-form-item>
+        <el-form-item label="首页图片:" prop="pic_top">
+          <el-switch v-model="temp.pic_top"></el-switch>
+        </el-form-item>
+        <el-form-item label="上传时间:" prop="pic_time">
+          <el-date-picker type="date" placeholder="选择时间" v-model="temp.pic_time" style="width:100%"></el-date-picker>
+        </el-form-item>
+        <el-form-item label="所属栏目:" prop="tag_name">
+          <el-select v-model="temp.tag_name" placeholder="选择栏目" style="width:100%">
+            <el-option label="区域一" value="shanghai"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      dialogFormVisible: true,
+      dialogStatus: '',
+      imageUrl: '',
+      temp: {
+        pic_name: '',
+        pic_con: '',
+        pic_url: '',
+        pic_fav: '',
+        pic_view: '',
+        pic_top: '',
+        pic_time: '',
+        tag_name: ''
+      }
+    }
+  },
+  methods: {
+    handleAvatarSuccess (res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw)
+    },
+    beforeAvatarUpload (file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
+    }
+  }
+}
+</script>
 <style rel="stylesheet/scss" lang="scss">
 .ph-img {
   position:relative;
@@ -90,5 +167,28 @@
       line-height:20px;
     }
   }
+}
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
 }
 </style>
