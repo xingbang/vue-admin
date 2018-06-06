3ff92952ba1fe3498990ef3c0a257b10b0735895
xingbang <suoxb@foxmail.com>
2018-06-06 16:51:20 +0800

    2018/6/6

--- src/views/photo/Photo.vue
+++ src/views/photo/Photo.vue
@@ -121,8 +121,12 @@ export default {
   methods: {
     // 上传文件到七牛云
     upqiniu (req) {
-      // console.log(req)
       let filetype = ''
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
       if (req.file.type === 'image/png') {
         filetype = 'png'
       } else {
@@ -131,31 +135,29 @@ export default {
       // 重命名要上传的文件
       const keyname = 'xingimg' + ((new Date()).valueOf()) + Math.floor(Math.random() * 100) + '.' + filetype
       // 从后端获取上传凭证token
-      axios({url: 'http://localhost:3000/photoUp', method: 'get'}).then((res) => {
+      axios.get('http://localhost:3000/photoUp').then((res) => {
         const formdata = new FormData()
         formdata.append('file', req.file)
         formdata.append('token', res.data)
         formdata.append('key', keyname)
         // 获取到凭证之后再将文件上传到七牛云空间
-        axios.post(this.domain, formdata, {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        }).then((resUrl) => {
+        axios.post(this.domain, formdata, config).then((resUrl) => {
           this.imageUrl = 'https://' + this.qiniuaddr + '/' + resUrl.data.key
-          console.log(this.imageUrl)
+          this.$message.success('图片上传成功！')
+          // console.log(this.imageUrl)
         })
       })
     },
+    // 上传前校验
     beforeUpload (file) {
       const isJPG = file.type === 'image/jpeg' || file.type === 'image/png'
       const isLt2M = file.size / 1024 / 1024 < 2
 
       if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!')
+        this.$message.error('上传图片只能是 JPG 格式!')
       }
       if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!')
+        this.$message.error('上传图片大小不能超过 2MB!')
       }
       return isJPG && isLt2M
     }
